069705e6d15d0ab51ffec3826b3caa1d412146b9
James Perkins <jamesperkins@hey.com>
2023-03-05 11:49:23 -0500

    Clerk with middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+import "./globals.css";
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs/app-beta";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
+    
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+        <head>
+          <title>Next.js 13 with Clerk</title>
+        </head>
+        <ClerkProvider>
+        <body>{children}</body>
+        </ClerkProvider>
     </html>
-  )
+  );
 }
